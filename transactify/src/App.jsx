4aad12d746248088439f73bfe07f4d6df9f87de1
nataliiasolomchak21 <natalia2001727@gmail.com>
2024-03-18 12:53:43 +0000

    feat: Structure main App component and components folder
    - Updated 'App.jsx' to structure the main application layout using Tailwind CSS classes and custom styles.
    - Created a 'components' folder with an 'index.js' file to facilitate the import of components into 'App.jsx'.
    - Added individual components for NavBar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials, and Hero.

--- transactify/src/App.jsx
+++ transactify/src/App.jsx
@@ -1,8 +1,36 @@
 import React from "react";
+import styles from './style'
+import { Billing, Business, CardDeal, Clients, CTA, Footer, NavBar, Stats, Testimonials, Hero } from "./components";
 
+// To transform words into components, click on first one word, hold Alt
+// Keep double-clicking on each new word
+// Press Ctrl + left arrow key, open the tag
+// Hold Ctrl + arrow right and close the component
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <div className=""></div>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <NavBar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+    <div className={`${styles.boxWidth}`}>
+      <Hero />
+    </div>
+    </div>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`${styles.boxWidth}`}>
+      <Stats />
+      <Business />
+      <Billing />
+      <CardDeal />
+      <Testimonials />
+      <Clients />
+      <CTA />
+      <Footer />
+    </div>
+    </div>
   </div>
 );
 
